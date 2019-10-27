2a94586bd2826c956daec9b6987c618ca75d87f4
Maxim Gelfand <gelfandmaxim@gmail.com>
2019-10-27 21:00:43 +0200

    Encryption works.

--- library/EncDec.hs
+++ library/EncDec.hs
@@ -1,15 +1,34 @@
-module EncDec (encrypt, decrypt) where
+module EncDec
+  ( encrypt
+  , decrypt
+  )
+where
 
-import ShiftRows
-import AddRoundKey
-import MixColumns
-import SBox
-import Utils
-import Globals
+import           ShiftRows
+import           AddRoundKey
+import           MixColumns
+import           SBox
+import           Utils
+import           Globals
+import           KeySchedule
+
+encrypt :: Key -> Block -> Block
+encrypt key block = helper (genSubKeys key) (addRoundKey key block)
+ where
+  helper :: [SubKey] -> Block -> Block
+  helper subKeys
+    | length subKeys == 1
+    = addRoundKey (head subKeys) . shiftRows . subBytes
+    | otherwise
+    = helper (tail subKeys)
+      . addRoundKey (head subKeys)
+      . mixColumns
+      . shiftRows
+      . subBytes
 
 
-encrypt = undefined
+decrypt :: Key -> Block -> Block
 decrypt = undefined
+-- encRound :: Key -> Block -> Block
 
-encRound :: Key -> Block -> Block
-encRound key = addRoundKey key . mixColumns . shiftRows . subBytes
+-- encRound key = addRoundKey key . mixColumns . shiftRows . subBytes
